65712da792181e2878af19324ebacbc389c45b8b
shodayuki <yshoda1227@gmail.com>
2021-02-06 16:38:37 +0900

    mainVisual 実装

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 $(function () {
+  const imgHeight = $('.js-header').outerHeight();
+  const header = $('.js-header');
+  $(window).on('load scroll', function(){
+      if ($(window).scrollTop() < imgHeight) {
+        header.removeClass('header--default');
+      }else {
+        header.addClass('header--default');
+      }
+  });
+
   $('.drawer').drawer();
 });
